bc595510839b18c0de8f2e878ff008ba0dd67d7e
GadCoder <german.ampuero@unmsm.edu.pe>
2025-07-09 16:02:29 -0500

    add headless:true for puppeter

--- src/clients/whatsapp-client.ts
+++ src/clients/whatsapp-client.ts
@@ -2,46 +2,52 @@ import * as qrcode from "qrcode-terminal";
 import { Client, ClientOptions, LocalAuth } from "whatsapp-web.js";
 
 class WhatsAppClient {
-    private static instance: WhatsAppClient;
-    public readonly client: Client;
+  private static instance: WhatsAppClient;
+  public readonly client: Client;
 
-    private constructor() {
-        const clientOptions: ClientOptions = {
-            authStrategy: new LocalAuth(),
-            webVersionCache: { type: "local" },
-        };
+  private constructor() {
+    const clientOptions: ClientOptions = {
+      authStrategy: new LocalAuth(),
+      webVersionCache: { type: "local" },
+      puppeteer: {
+        headless: true,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      },
+    };
 
-        if (process.env.PUPPETEER_EXECUTABLE_PATH) {
-            clientOptions.puppeteer = {
-                executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-                args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            };
-        }
-        this.client = new Client(clientOptions);
+    if (process.env.PUPPETEER_EXECUTABLE_PATH) {
+      clientOptions.puppeteer!.executablePath =
+        process.env.PUPPETEER_EXECUTABLE_PATH;
+    }
+    this.client = new Client(clientOptions);
+  }
+
+  public async initialize(): Promise<void> {
+    console.log("Initializing WhatsApp client...");
+
+    if (!process.env.PUPPETEER_EXECUTABLE_PATH) {
+      this.client.on("qr", (qr: string) => {
+        qrcode.generate(qr, { small: true });
+      });
     }
 
-    public async initialize(): Promise<void> {
-        console.log("Initializing WhatsApp client...");
+    this.client.on("ready", () => {
+      console.log("Client is ready!");
+    });
 
-        if (!process.env.PUPPETEER_EXECUTABLE_PATH) {
-            this.client.on("qr", (qr: string) => {
-                qrcode.generate(qr, { small: true });
-            });
-        }
+    await this.client.initialize();
+  }
 
-        this.client.on("ready", () => {
-            console.log("Client is ready!");
-        });
-
-        await this.client.initialize();
+  public static getInstance(): WhatsAppClient {
+    if (!WhatsAppClient.instance) {
+      WhatsAppClient.instance = new WhatsAppClient();
     }
-
-    public static getInstance(): WhatsAppClient {
-        if (!WhatsAppClient.instance) {
-            WhatsAppClient.instance = new WhatsAppClient();
-        }
-        return WhatsAppClient.instance;
-    }
+    return WhatsAppClient.instance;
+  }
 }
 
 export default WhatsAppClient.getInstance();
